b78aa4aa60c7e0188e3df1bfc285c024761480d7
nphuoctho <nguyenphuoctho0310@gmail.com>
2025-07-13 23:56:20 +0700

    chore: update neovim from devaslife

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,37 +1,67 @@
 return {
-  -- add more treesitter parsers
+  { "nvim-treesitter/playground", cmd = "TSPlaygroundToggle" },
+
   {
     "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
     opts = {
       ensure_installed = {
-        "bash",
-        "diff",
-        "html",
-        "javascript",
-        "jsdoc",
-        "json",
-        "jsonc",
-        "lua",
-        "luadoc",
-        "luap",
-        "markdown",
-        "markdown_inline",
-        "printf",
-        "python",
-        "query",
-        "regex",
-        "toml",
-        "tsx",
-        "typescript",
-        "vim",
-        "vimdoc",
-        "xml",
-        "yaml",
+        "astro",
+        "cmake",
+        "cpp",
+        "css",
+        "fish",
+        "gitignore",
+        "go",
+        "graphql",
+        "http",
+        "java",
+        "php",
+        "rust",
+        "scss",
+        "sql",
+        "svelte",
       },
-      sync_install = false,
-      hightlight = { enable = true },
-      indent = { enable = true },
+
+      -- matchup = {
+      -- 	enable = true,
+      -- },
+
+      -- https://github.com/nvim-treesitter/playground#query-linter
+      query_linter = {
+        enable = true,
+        use_virtual_text = true,
+        lint_events = { "BufWrite", "CursorHold" },
+      },
+
+      playground = {
+        enable = true,
+        disable = {},
+        updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+        persist_queries = true, -- Whether the query persists across vim sessions
+        keybindings = {
+          toggle_query_editor = "o",
+          toggle_hl_groups = "i",
+          toggle_injected_languages = "t",
+          toggle_anonymous_nodes = "a",
+          toggle_language_display = "I",
+          focus_language = "f",
+          unfocus_language = "F",
+          update = "R",
+          goto_node = "<cr>",
+          show_help = "?",
+        },
+      },
     },
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+
+      -- MDX
+      vim.filetype.add({
+        extension = {
+          mdx = "mdx",
+        },
+      })
+      vim.treesitter.language.register("markdown", "mdx")
+    end,
   },
 }
